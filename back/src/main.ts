e0368bdccdd8d6cd2ae6551b88677dc80795b84d
Ana María Vargas Mejía <149446905+anamaria1215@users.noreply.github.com>
2025-10-04 02:16:29 -0500

    Update main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerGlobal } from './middleware/loggerGlobal';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(new LoggerGlobal().use.bind(new LoggerGlobal()));
+  await app.listen(3002);
+  console.log('Servidor corriendo en el puerto 3002 🐱');
 }
 bootstrap();
