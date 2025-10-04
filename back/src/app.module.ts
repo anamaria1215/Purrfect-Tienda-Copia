5931143f06c7a9328acadbdcb5a660890e9c0ca3
Ana María Vargas Mejía <149446905+anamaria1215@users.noreply.github.com>
2025-10-04 02:43:31 -0500

    Update app.module.ts

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,15 +1,10 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerGlobal } from './loggerGlobal';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerGlobal).forRoutes('*');
-  }
-}
+export class AppModule {}
