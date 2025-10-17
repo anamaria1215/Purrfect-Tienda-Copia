1ac356704de68f78d869dbd4469410ed487919e9
Ana Maria <vargas.anamaria1215@gmail.com>
2025-10-16 21:26:40 -0500

    Agregar entidades, enums y estructura inicial

---
+++ back/src/enum/statusOrder.enum.ts
@@ -0,0 +1,10 @@
+export enum StatusOrder {
+    CREATED = 'Created',
+    PENDING = 'Pending',
+    CONFIRMED = 'Confirmed',
+    RETURNED = 'Returned',
+    FAILED = 'Failed',
+    CANCELLED = 'Cancelled',
+    FINISHED = 'Finished',
+  }
+  
