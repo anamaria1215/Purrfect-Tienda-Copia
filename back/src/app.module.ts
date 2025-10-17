16651602451712a2fdffaac1ad45afae2cffd886
Ana María Vargas Mejía <149446905+anamaria1215@users.noreply.github.com>
2025-10-17 00:57:09 -0500

    Update app.module.ts

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,28 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import ormConfig from './config/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.development', // asegura que use este archivo
       isGlobal: true,
+      envFilePath: '.env',
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
+        port: Number(configService.get<number>('DB_PORT')),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
-        entities: ['dist/**/*.entity.js'],
-        synchronize: false,
+        entities: ormConfig.entities,
+        synchronize: true,
         logging: true,
       }),
     }),
   ],
 })
 export class AppModule {}
+
