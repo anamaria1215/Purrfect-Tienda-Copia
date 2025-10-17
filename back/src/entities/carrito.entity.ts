a1ee393bb99ec61260953d1db3e3bca910bacf70
Ana María Vargas Mejía <149446905+anamaria1215@users.noreply.github.com>
2025-10-16 19:20:02 -0500

    Update carrito.entity.ts

--- back/src/entities/carrito.entity.ts
+++ back/src/entities/carrito.entity.ts
@@ -1,38 +1,18 @@
-import { Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn, JoinColumn } from 'typeorm';
-import { Credential } from './credential.entity';
-import { Order } from './orders.entity';
-import { Carrito } from './carrito.entity';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './users.entity';
+import { CarritoProducto } from './carrito_producto.entity';
 
-@Entity({ name: 'users' })
-export class User {
+@Entity({ name: 'carritos' })
+export class Carrito {
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
-  @Column({ type: 'varchar', length: 100 })
-  name: string;
+  @ManyToOne(() => User, (user) => user.carritos)
+  user: User;
 
-  @Column({ type: 'varchar', length: 100, unique: true })
-  email: string;
+  @Column({ type: 'date' })
+  creationDate: Date;
 
-  @Column({ type: 'varchar', length: 255 })
-  password: string;
-
-  @Column({ type: 'varchar', length: 15, nullable: true })
-  phone: string;
-
-  @Column({ type: 'varchar', length: 255, nullable: true })
-  address: string;
-
-  @Column({ type: 'varchar', length: 50, default: 'user' })
-  role: string;
-
-  @OneToOne(() => Credential, (credential) => credential.user)
-  @JoinColumn()
-  credential: Credential;
-
-  @OneToMany(() => Order, (order) => order.user)
-  orders: Order[];
-
-  @OneToMany(() => Carrito, (carrito) => carrito.user)
-  carritos: Carrito[];
+  @OneToMany(() => CarritoProducto, (cp) => cp.carrito)
+  carritoProductos: CarritoProducto[];
 }
