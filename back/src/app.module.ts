0b52c90407d3ba01d5c804692c6590cabe01bbda
Ana María Vargas Mejía <149446905+anamaria1215@users.noreply.github.com>
2025-10-17 02:18:28 -0500

    Update app.module.ts
    Removed TypeORM and ConfigModule imports, simplifying the AppModule.

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,30 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import ormConfig from './config/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: Number(configService.get<number>('DB_PORT')),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: ormConfig.entities,
-        synchronize: true,
-        logging: true,
-      }),
-    }),
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
-
